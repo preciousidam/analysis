23b93f1dfc0a808558ce220d6c24bf52fee9c27e
Precious Idam <pidam@keystonebankng.com>
2020-11-13 17:28:18 +0100

    ...

--- pages/index.js
+++ pages/index.js
@@ -91,49 +91,7 @@ export function Home({}){
                         </Paper>
                     </div>
                 </div>
-                <div className="card">
-                    <span>IKOYI</span>
-                    <Title level={3}>{properties.length}</Title>
-                </div>
             </div>
-            <div id="table-chart">
-                <Paper id="float-left">
-                    <header>
-                        <h3>Average rent</h3>
-                        <SelectInput 
-                            id="sBedroom" 
-                            value={bed}
-                            onChange={e => setBed(e.target.value)}
-                            label=""
-                            options={[
-                                {text: '1 Bedroom', value:1},
-                                {text: '2 Bedroom', value:2},
-                                {text: '3 Bedroom', value:3},
-                                {text: '4 Bedroom', value:4},
-                            ]} 
-                        />
-                    </header>
-                    <PriceChart />
-                </Paper>
-                <Paper id="float-right">
-                    <div 
-                        style={{
-                            padding: 20, 
-                            display: "flex", 
-                            flexDirection: 'row', 
-                            justifyContent: "space-between", 
-                            alignItems: "center"
-                        }}
-                    >
-                        <Search placeholder="Search" />
-                        <Link href="/properties"><a>View All<FontAwesomeIcon icon='arrow-right' style={{marginLeft: 5}} /></a></Link>
-                    </div>
-                    <PropertyList />
-                </Paper>
-            </div>
-            <Paper id="bedPriceCont">
-                
-            </Paper>
         </MainLayout>
     )
 }
